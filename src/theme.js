528c36b40e673868487716d9bc7e648e1bd6e74a
Sok-studios <info.sokstudios@gmail.com>
2026-06-13 11:12:56 +1200

    Add files via upload

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const C={
+  bg:'#FAF8F4',
+  dark:'#231410',
+  mid:'#6B4D3A',
+  gold:'#9A7A42',
+  goldLight:'#EEE2C8',
+  red:'#B04A2A',
+  border:'#E2D4C4',
+}
+
+// RIDIBatang as universal heading font (EN + KO both)
+export const fK="'RIDIBatang','Cormorant Garamond',Georgia,serif"
+export const fH="'RIDIBatang','Cormorant Garamond',Georgia,serif"
+export const fB="'RIDIBatang','Cormorant Garamond',Georgia,serif"
